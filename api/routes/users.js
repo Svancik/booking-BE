bdcfa49ebeb1a2a0e43e145629a302688de2f420
Svancik <thesvancik@gmail.com>
2022-11-22 20:47:35 +0100

    jwt & cookie authoritazion & verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,34 @@
 import express from "express";
+import { deleteUser, updateUser, getUser, getUsers } from './../controllers/user.js';
+import { verifyToken, verifyUser, verifyAdmin } from './../utils/verifyToken.js';
 
 const router = express.Router();
+
+// //toto proběhne jako 1.
+// router.get("/checkauthentication", verifyToken, (req,res,next) =>{
+//     res.send("hello user, you are logged in.")
+// });
+
+// //toto proběhne jako 2. pomocí next() z 1. kroku
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//     res.send("hello user, you are logged in and you can delete your account.")
+// });
+
+// //toto proběhne jako 3.
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next) =>{
+//     res.send("hello admin, you are logged in and you can delete all accounts.")
+// });
+
+
+//Níže přidáme middleware verifikační fce výše abychom provedli verifikaci před vykonáním jednotlivých úkonů jako je update, delete a get
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+//GET
+router.get("/:id", verifyUser, getUser);
+//GET ALL HOTELS
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
